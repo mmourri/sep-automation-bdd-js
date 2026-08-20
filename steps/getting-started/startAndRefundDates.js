a75a3338574b1238f9ccfa89381be6b3d297d67c
mmourri <mmourri@gmail.com>
2026-08-20 12:32:37 -0700

    Start And Refund Feature

--- steps/getting-started/startAndRefundDates.js
+++ steps/getting-started/startAndRefundDates.js
@@ -1,5 +1,34 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
 
+// Given("user is on the enrollment page", async function () {
+//     await startApplicationPage.login();
+// });
+
+Then("the program start date in displayed", async function () {
+    await expect(startApplicationPage.programStartDate).toBeVisible();
+});
+
+Then("the program refund date in displayed", async function () {
+    await expect(startApplicationPage.refundEndDate).toBeVisible();
+});
+
+Then("the displayed start date for the program is correct", async function () {
+    const ACTUAL_START_DATE = await startApplicationPage.programStartDate.innerText();
+    const EXPECTED_START_DATE = productInfo.startDate;
+    // console.log(`Actual Start Date: ${ACTUAL_START_DATE}`);
+    // console.log(`Expected Start Date: ${EXPECTED_START_DATE}`);
+    // console.log(`Expected Upfront Price: ${productInfo.prices[0].baseAmount}`);
+    // console.log(`Expected Upfront Discount Amount: ${productInfo.prices[0].upfrontDiscountAmount}`);
+    expect(ACTUAL_START_DATE).toBe(EXPECTED_START_DATE);
+});
+
+Then("the displayed refund date for the program is correct", async function () {
+    const ACTUAL_REFUD_DATE = await startApplicationPage.refundEndDate.innerText();
+    const EXPECTED_REFUD_DATE = productInfo.refundDate;
+    // console.log(`Actual Refund Date: ${ACTUAL_REFUD_DATE}`);
+    // console.log(`Expected Refund Date: ${EXPECTED_REFUD_DATE}`);
+    expect(ACTUAL_REFUD_DATE).toBe(EXPECTED_REFUD_DATE);
+});
